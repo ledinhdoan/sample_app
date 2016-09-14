302fdef6f543438c50b06ad7f3d90b0da18ab0ab
LeDinhDoan <ledinhdoan.0083@gmail.com>
2016-09-14 14:58:21 +0700

    Finish lo in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   root  'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
   get 'about' => 'static_pages#about'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users 
 
 end
